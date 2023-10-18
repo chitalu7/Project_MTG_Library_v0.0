95a21401049913c9c805ef6e52002c4ebe3666ef
chitalu7 <chitalu.am@gmail.com>
2023-10-18 14:36:46 -0700

    All MTG files uploaded

---
+++ MTGCards_Pagination/MTGArenaWebAPI/MTGArenaWebAPI/MTGArenaCardsResponse.cs
@@ -0,0 +1,11 @@
+﻿using MTGArenaWebAPI.Data.Entities;
+
+namespace MTGArenaWebAPI
+{
+    public class MTGArenaCardsResponse
+    {
+        public List<MTGArenaCards> MTGArenaCards { get; set; } = new List<MTGArenaCards>();
+        public int Pages { get; set; }  
+        public int CurrentPage { get; set; }    
+    }
+}
